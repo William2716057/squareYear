0400580dbc92f49539e2eda093b98ec588e47694
William <willspri@gmail.com>
2024-12-23 09:17:41 +1300

    print statement changed

--- squareYear/squareYear.py
+++ squareYear/squareYear.py
@@ -2,5 +2,5 @@ import math
 
 year1, year2 = 0, 3000
 squares = filter(lambda year: math.isqrt(year) ** 2 == year, range(year1, year2))
-print("Years that are square numbers: ")
+print(f"Years that are square numbers between: {year1} and {year2}")
 print(*squares)
