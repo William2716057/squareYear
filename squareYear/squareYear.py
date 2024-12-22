4c3c50460a420919a17004ffde92d478fc1036fa
William <willspri@gmail.com>
2024-12-23 09:15:48 +1300

    tidied

--- squareYear/squareYear.py
+++ squareYear/squareYear.py
@@ -1,7 +1,6 @@
 import math
 
-year1 = 0
-year2 = 3000
+year1, year2 = 0, 3000
 squares = filter(lambda year: math.isqrt(year) ** 2 == year, range(year1, year2))
 print("Years that are square numbers: ")
 print(*squares)
