ed11d07da85b5c5730d3b425c9992e7011bcc2d9
William <willspri@gmail.com>
2024-12-23 09:14:46 +1300

    years changed to variables

--- squareYear/squareYear.py
+++ squareYear/squareYear.py
@@ -1,5 +1,7 @@
 import math
 
-squares = filter(lambda year: math.isqrt(year) ** 2 == year, range(0, 3000))
+year1 = 0
+year2 = 3000
+squares = filter(lambda year: math.isqrt(year) ** 2 == year, range(year1, year2))
 print("Years that are square numbers: ")
 print(*squares)
