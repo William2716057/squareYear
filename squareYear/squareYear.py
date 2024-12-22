8746b69f3324bfcc75bcd7dd2612418c97a1cc06
William <willspri@gmail.com>
2024-12-23 09:10:21 +1300

    comments removed

--- squareYear/squareYear.py
+++ squareYear/squareYear.py
@@ -4,9 +4,3 @@ squares = filter(lambda year: math.isqrt(year) ** 2 == year, range(0, 3000))
 
 for year in squares:
     print(year)
-
-#for year in range(0, 3000):
-#    result = math.isqrt(year)
-
-#    if result * result == year:
-#        print(year)
