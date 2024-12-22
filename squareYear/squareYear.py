ec59484885794068d99c6b35ead34ef26c2cce5b
William <willspri@gmail.com>
2024-12-23 09:11:37 +1300

    simplified

--- squareYear/squareYear.py
+++ squareYear/squareYear.py
@@ -1,6 +1,6 @@
 import math
 
 squares = filter(lambda year: math.isqrt(year) ** 2 == year, range(0, 3000))
-
-for year in squares:
-    print(year)
+print(*squares)
+#for year in squares:
+#    print(year)
